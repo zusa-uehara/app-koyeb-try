4ed66e50cdb5865c06132352c8811bb495dc942b
root <root@zoosa.localdomain>
2025-08-31 16:30:07 +0900

    PostgreSQL環境変数修正

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-// あなたの接続文字列から情報を抽出
-$url = "postgres://user:npg_lTNujZLcI7q5@ep-rough-hall-a4ktsifc.us-east-1.pg.koyeb.app/koyebdb";
-
-$url_parts = parse_url($url);
-$host = $url_parts['host'];
-$user = $url_parts['user'];
-$password = $url_parts['pass'];
-$dbname = ltrim($url_parts['path'], '/');
+// 環境変数からデータベースの接続情報を取得
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 // DSN文字列を作成
 $dsn = "pgsql:host=$host;dbname=$dbname;user=$user;password=$password";
